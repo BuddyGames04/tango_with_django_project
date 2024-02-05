0fbc55501fe5820c4e283e71acd16f0704374ad6
Luke Cochrane <2767178c@student.gla.ac.uk>
2024-02-05 13:44:31 +0000

    Chapter 5 exercises completed

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+# Define a custom admin class for the Page model
+class PageAdmin(admin.ModelAdmin):
+    # This attribute specifies the order in which fields should appear in the list display.
+    list_display = ( 'title', 'category', 'url')
+
+# Register the Category model with the default admin interface
+admin.site.register(Category)
+
+# Register the Page model with the custom admin interface
+admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -11,14 +11,9 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def test(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
     context_dict = {'boldmessage': 'Godot, Unity, Unreal, GMS2, Construct'}
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/test.html', context=context_dict)
     
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/index/'>Index</a>" )
+    return render(request, 'rango/about.html')
